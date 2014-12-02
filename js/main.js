68ec38f33775b25049fba8e63aec95f130cfd81d
Jonathan Koser <jkoser@wisc.edu>
2014-12-02 01:21:37 -0600

    Data testing
    Added in some line data and tried to figure out how to visualize it a
    little bit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //global variables
-var currentYear = 1903;
+var currentYear = 1906;
 
 //begin script when window loads
 window.onload = initialize();
@@ -18,6 +18,10 @@ function setMap () {
     var mapContainer = d3.select("body")
             .append("div")
             .attr("class", "mapContainer");
+    
+    mapContainer.append("text")
+        .attr("class", "displayYear")
+        .text(currentYear);
     
     //create a new svg element with the above dimensions
     var map = d3.select(".mapContainer")
@@ -67,10 +71,11 @@ function setMap () {
     //use queue.js to parallelise asynchronous data loading
     queue()
         .defer(d3.json, "data/landOutline.topojson") //load attributes from csv
+        .defer(d3.json, "data/Test_Lines.topojson")
         .await(callback); //trigger callback function once data is loaded
    
     //retrieve and process NZ json file and data
-    function callback(error, land) {
+    function callback(error, land, lines) {
 
         var landMasses = map.selectAll(".landMasses") 
                 .data(topojson.feature(land, land.objects.landOutline).features)
@@ -79,17 +84,50 @@ function setMap () {
                 .attr("class", "landMasses") //assign class for styling
                 .append("path")
                 .attr("class", "landBody")
-                .attr("d", path); //project data as geometry in svg
-    
-        displayYear();
+                .attr("d", path); //project data as geometry in svg 
+
+        
+        var lines = map.selectAll(".lines")
+                .data(topojson.feature(lines, lines.objects.Test_Lines).features)
+                .enter()
+                .append("g")
+                .attr("class", "lines")
+                .append("path")
+                .attr("class", function (d) {return d.properties.Year})
+                .attr("d", path)
+                .on("click", function (d) {
+                    console.log(d.properties.Descrip);
+                })
+                .on("mouseover", function() {console.log("over")});
+        
+        
+        updateLines();
     };//end callback
+    
     
 };// end setMap
 
-function displayYear() {
-    var mapContainer = d3.select(".mapContainer");
-    mapContainer.append("text")
-        .attr("class", "displayYear")
-        .text(currentYear);
+function updateYear() {
+    var yearText = d3.select(".displayYear");
+    yearText.text(currentYear);
 }//end display year
+
+//function to update which lines are being displayed
+//the only problem I have with this is that I'm unconfortable
+//with the fact that I'm drawing all the features first then
+//just making the ones we're not using invisible. I feel like
+//there must be a way to just selectively draw the features
+//based on the year
+function updateLines() {
+    var lines = d3.selectAll(".lines")
+            .style("stroke", function (d) {
+                if (d.properties.Year == currentYear) {
+                    return "black";
+                }
+                else {
+                    return "none";
+                }
+             });
+    
+}; //end update lines
 
